d7f35f9c4a6ee597323c548fb63b7684c0617775
cpdjvks <151480550+cpdjvks@users.noreply.github.com>
2023-11-24 18:06:32 +0900

    add

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
 import streamlit as st
 
 from app_home import run_home_app
+from app_eda import run_eda_app
 
 
 def main() :
-    st.title('전국 오피스텔 월세 가격')
+    st.title('전국 오피스텔 중위 월세 가격')
 
     menu = ['Home', 'eda']
 
     choice = st.sidebar.selectbox('메뉴 선택', menu)
 
     if choice == menu[0] :
-        run_home_app
+        run_home_app()
     elif choice == menu[1] :
         run_eda_app()
 
--- app_eda.py
+++ app_eda.py
@@ -6,4 +6,22 @@ import matplotlib.pyplot as plt
 
 def run_eda_app() :
 
-    st.subheader('')
+    st.subheader('전체 데이터')
+
+    st.text('단위 : 천원')
+
+    df = pd.read_csv('./data/house.csv')
+
+    st.dataframe(df)
+
+    column_list = df.columns[1: ]
+
+    selected_column = st.selectbox('년월을 선택하세요', column_list)
+
+    st.text(selected_column + ' 가장 싼 지역')
+    st.dataframe(df.loc[df[selected_column] == df[selected_column].min(), ])
+
+    st.text(selected_column + ' 가장 비싼 지역')
+    st.dataframe(df.loc[df[selected_column] == df[selected_column].max(), ])
+
+    st.dataframe(df.loc[0,])
