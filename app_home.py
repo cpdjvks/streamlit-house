87cca47298e12f2712dfa6718e410ea700b3b98d
cpdjvks <151480550+cpdjvks@users.noreply.github.com>
2023-11-27 16:24:59 +0900

    Update app_home.py

--- app_home.py
+++ app_home.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 def run_home_app():
     st.subheader('Home')
-    st.text('전국 오피스텔의 중위 월세 가격을 분석한 앱입니다.')
+    st.text('전국 오피스텔의 중위 월세 가격을 분석한 앱입니다!')
 
     st.text('기간은 2020년 7월 부터 2023년 6월까지입니다.')
 
